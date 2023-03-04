635ef776beca67ee60bb95a5067dbb5faeff87ab
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2023-03-04 18:57:53 -0500

    Aleksandr's Model

---
+++ aleksandr.py
@@ -0,0 +1,61 @@
+import sys
+import logging
+import re
+import openai
+import os
+from dotenv import load_dotenv
+import os
+load_dotenv()
+key = os.getenv("OPENAI")
+
+input_1 = sys.argv[1]
+
+#
+# A function to remove URLs
+#
+def remove_urls(text):
+    return re.sub(r'http\S+', '', text)
+
+#
+# A function to mentions from tweets 
+#
+def remove_mentions(text):
+  return re.sub(r'@\S+', '', text)
+
+#
+# A function to remove special characters
+#
+def remove_special_characters(text):
+  return re.sub(r'[^\w\s]', '', text)
+
+
+def clean_tweet(text):
+    text = remove_urls(text)
+    text = remove_mentions(text)
+    text = remove_special_characters(text)
+    return text
+
+
+def getPrompt(prompt_text):
+    
+    question = f'''
+    The topic is ChatGPT. Based on the following text, identify if the author is confident in ChatGPT / not confident in ChatDPT / neutral.
+    Text: "{prompt_text}"
+
+    Answer (one word lowercase):
+    '''
+    
+    return question
+
+
+openai.api_key = key
+
+prompt = getPrompt(clean_tweet(input_1))
+
+response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0)
+
+answer = str(response.choices[0].text)
+answer = answer.replace(" ", "")
+answer = answer.replace("notconfident", "not confident")
+
+print(answer)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
-
+const { spawn } = require("child_process");
+const os = require("os");
 const app = express();
 const PORT = 9000;
 
@@ -12,28 +13,69 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get("/aleksandr", (req, res) => {
-  input = req.query.input; 
-  if (!input || input == "" ) {
-    res.send("Hello Aleksandr. Please add parameter to the end of the url: /aleksandr?input=DATA");
+function runPythonScript(pythonV, file, input) {
+  return new Promise((resolve, reject) => {
+    let dataToSend = "";
+
+    python = spawn(pythonV, [file, input]);
+    
+    python.stdout.on("data", function (data) {
+      console.log("Python Child Process Data: ", data.toString());
+      dataToSend += data.toString();
+    });
+
+    python.on("exit", (code, signal) => {
+      if (code) {
+        console.error("Child exited with code: ", code);
+        reject(new Error(`Child exited with code ${code}`));
+      } else if (signal) {
+        console.error("Child was killed with signal: ", signal);
+        reject(new Error(`Child was killed with signal ${signal}`));
+      } else {
+        console.log("Child exited okay");
+        resolve(dataToSend);
+      }
+    });
+
+    python.on("close", (code) => {
+      // stream from child process is closed
+    });
+  });
+}
+
+async function GPT3_5(input) {
+  return await runPythonScript("python3", "aleksandr.py", input);
+}
+
+app.get("/aleksandr", async (req, res) => {
+  input = req.query.input;
+  if (!input || input == "") {
+    res.send(
+      "Hello Aleksandr. Please add parameter to the end of the url: /aleksandr?input=DATA"
+    );
   } else {
-    res.send("Coming soon");
+    response = await GPT3_5(input);
+    res.send(response);
   }
 });
 
 app.get("/duure_mall/", (req, res) => {
-  input = req.query.input
-  if (!input || input == "" ) {
-    res.send("Hello Duurenbayar and Mallory. Please add parameter to the end of the url: /duure_mall?input=DATA");
+  input = req.query.input;
+  if (!input || input == "") {
+    res.send(
+      "Hello Duurenbayar and Mallory. Please add parameter to the end of the url: /duure_mall?input=DATA"
+    );
   } else {
     res.send("Coming soon");
   }
 });
 
 app.get("/derik", (req, res) => {
-  input = req.query.input; 
-  if (!input || input == "" ) {
-    res.send("Hello Derik. Please add parameter to the end of the url: /derik?input=DATA");
+  input = req.query.input;
+  if (!input || input == "") {
+    res.send(
+      "Hello Derik. Please add parameter to the end of the url: /derik?input=DATA"
+    );
   } else {
     res.send("Coming soon");
   }
