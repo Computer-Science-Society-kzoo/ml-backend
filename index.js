2f9df3d943b593633aaf293f4953c6f343fce3e5
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2023-03-04 18:06:26 -0500

    Init URLs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 const PORT = 9000;
@@ -9,4 +10,31 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/aleksandr", (req, res) => {
+  input = req.query.input; 
+  if (!input || input == "" ) {
+    res.send("Hello Aleksandr. Please add parameter to the end of the url: /aleksandr?input=DATA");
+  } else {
+    res.send("Coming soon");
+  }
+});
+
+app.get("/duure_mall/", (req, res) => {
+  input = req.query.input
+  if (!input || input == "" ) {
+    res.send("Hello Duurenbayar and Mallory. Please add parameter to the end of the url: /duure_mall?input=DATA");
+  } else {
+    res.send("Coming soon");
+  }
+});
+
+app.get("/derik", (req, res) => {
+  input = req.query.input; 
+  if (!input || input == "" ) {
+    res.send("Hello Luke. Please add parameter to the end of the url: /derik?input=DATA");
+  } else {
+    res.send("Coming soon");
+  }
 });
