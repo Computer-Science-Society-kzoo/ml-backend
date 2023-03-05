362472ab12261779d6d43a4987d41cbd44c1fddf
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2023-03-04 19:03:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { spawn } = require("child_process");
 const os = require("os");
 const app = express();
 const PORT = 9000;
-const pythonVersion = "python";
+const pythonVersion = "python3";
 
 app.get("/", (req, res) => {
   res.send("Hello World");
